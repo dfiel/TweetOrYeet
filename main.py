f251746b9ea44b533854a6feea928b5a039342b0
dfiel <github@dfiel.com>
2020-02-22 23:47:01 -0500

    Basic main function skeleton

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import os
+import encryption, generate_key
 
 
 def grab_api():
@@ -23,8 +24,18 @@ def trav_dirs(rootDir):
             print(os.path.abspath(x))
 
 
+def main():
+    encrypter = encryption.Encrypter()
+    if os.path.exists('fernet.key'):
+        encrypter.load_key('fernet.key')
+    else:
+        generate_key.generate_fernet_key()
+
+
 
 if __name__ == '__main__':
+    encrypter = encryption.Encrypter()
+    encrypter.load_key('fernet.key')
     print(grab_latest_tweet())
     tweet_time = grab_latest_tweet()
     print(check_tweet_time(tweet_time[0]))
